6b85db54f0020c0808f0adae0ec83e428e92e78f
SergeyKardashev <angeldeep@yandex.ru>
2024-06-03 17:47:49 +0700

    feat: add test cases for Lodash.groupBy

--- 02_sync/sync.js
+++ 02_sync/sync.js
@@ -3,7 +3,27 @@ class Lodash {
 		return array.filter(val => !!val)
 	}
 	
-	groupBy(array, prop) {}
+	groupBy(array, prop) {
+		// Arguments are array and function. Expect to get an object.
+		// Keys are to be something that function returns
+		return array.reduce((acc, i) => {
+			// Reduce takes: 1) callback, 2) initial state of accumulator (empty object)
+			// Callback takes: 1) accumulator 'acc', 2) current value 'i' to iterate.
+
+			// If 'prop' is a function, run function with argument 'i'
+			// else run method 'prop' for item 'i'
+			const key = typeof prop === 'function' ? prop(i) : i[prop]; 
+			// If there is no such key in accumulator, 
+			// then create the key and assign an empty array to it.
+			if (!acc[key]) {
+				acc[key] = []; // Now the accumulator has the key with empty array.
+			}
+			acc[key].push(i); // Put current item to the key of accumulator
+			return acc; // Return accumulator after all iterations.
+
+		}, {});
+
+	}
 }
 
 module.exports = Lodash;
--- 02_sync/sync.spec.js
+++ 02_sync/sync.spec.js
@@ -1,18 +1,16 @@
 const Lodash = require('./sync');
 
+let _ = new Lodash(); //  class instance with name '_'
+
 describe('Lodash: compact', () => {
 
-    let _ = new Lodash(); //  class instance with name '_'
     let array;
 
     beforeEach(() => {
         array = [false, 42, 0, '', true, null, 'hello'];
      });
 
-    // afterAll(() => {
-    //     _ = new Lodash();
-    // })
-
+    // afterAll(() => { _ = new Lodash(); }) // just an example
 
     test('should be defined', () => { 
         expect(_.compact).toBeDefined();
@@ -20,7 +18,7 @@ describe('Lodash: compact', () => {
     });
 
     test('should working array be editable', () => {
-        array.push(...['one', 'two']);
+        array.push(...['one', 'two']); // pay attention to '...'
         expect(array).toContain('one');
         expect(array).toContain('two');
     });
@@ -28,8 +26,7 @@ describe('Lodash: compact', () => {
     test('should remove falsy values from array', () => {
         const result = [42, true, 'hello'];
         
-        // expect(_.compact(array)).toBe(result); // does not work for array. Only for primitives
-        expect(_.compact(array)).toEqual(result);
+        expect(_.compact(array)).toEqual(result); // toBe(result) NOT work. Only for primitives
     });
 
     test('should not contain falsy values', () => {
@@ -37,5 +34,36 @@ describe('Lodash: compact', () => {
         expect(_.compact(array)).not.toContain(0);
         expect(_.compact(array)).not.toContain('');
         expect(_.compact(array)).not.toContain(null);
+    });
+});
+
+describe('Lodash: groupBy', () => {
+
+    test('should be defined', () => { 
+        expect(_.groupBy).toBeDefined();
+        expect(_.groupBy).not.toBeUndefined();
+    });
+
+    test('should group array items by Math.floor', () => {
+        const array = [2.2, 2.4, 4.2, 3.1];
+        const result = {
+            2: [2.2, 2.4],
+            4: [4.2], 
+            3: [3.1]
+        }
+        expect(_.groupBy(array, Math.floor)).toEqual(result);
+    });
+
+    test('should group array items by length', () => {
+        const array = ['one', 'two', 'three'];
+        const result = {
+            5: ['three'],
+            3: ['one', 'two']
+        }
+        expect(_.groupBy(array, 'length')).toEqual(result);
+    });
+
+    test('should NOT return array', () => {
+        expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array); // Array - class
     });
 });
