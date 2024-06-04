886ebd0512192a308f5f4dfcf8b4285661d90532
SergeyKardashev <angeldeep@yandex.ru>
2024-06-04 23:09:00 +0700

    feat: add reject promise test

--- 01_intro/03_async/async.spec.js
+++ 01_intro/03_async/async.spec.js
@@ -1,17 +1,32 @@
-const Ajax = require('./async');
+const Ajax = require("./async");
 
-describe('Ajax: echo', () => {
+describe("Ajax: echo", () => {
+  // async based test of resolve
+  test("should return value async", async () => {
+    const result = await Ajax.echo("some data");
+    expect(result).toBe("some data");
+  });
 
-    // async based test
-    test('should return value async', async () => { 
-        const result = await Ajax.echo('some data');
-        expect(result).toBe('some data');
+  // promise based test of resolve
+  test("should return value with promise", () => {
+    return Ajax.echo("some data").then((data) => {
+      expect(data).toBe("some data");
     });
+  });
 
-    // promise based test
-    test('should return value with promise', () => { 
-        return Ajax.echo('some data').then(data => {
-            expect(data).toBe('some data')
-        });
+  // promise based test of reject with promise
+  test("should return 'error' with promise", () => {
+    return Ajax.echo().catch((err) => {
+      expect(err).toBeInstanceOf(Error);
     });
-})
+  });
+
+  // async based test of reject with async
+  test("should return 'error' with async", async () => {
+    try {
+      await Ajax.echo();
+    } catch (error) {
+      expect(error.message).toBe("error");
+    }
+  });
+});
