b61af8f508d9670c245eef66fe94194b3a407e74
SergeyKardashev <angeldeep@yandex.ru>
2024-06-06 13:22:34 +0700

    feat: add test to check if callback works well

--- 04_mock/mock.test.js
+++ 04_mock/mock.test.js
@@ -7,18 +7,24 @@ describe('Map function', () => {
     beforeEach(() => {
         array = [1, 2, 3, 5];
         fn = jest.fn(x => x ** 2);
+        map(array, fn);
     });
 
     test('should call callback', () => {
-        map(array, fn);
         // expect(fn).toBeCalled(); // toBeCalled is deprecated. Use toHaveBeenCalled
         expect(fn).toHaveBeenCalled();
     });
 
     test('should call callback 4 times', () => {
-        map(array, fn);
         expect(fn).toHaveBeenCalledTimes(4); // option 1
         expect(fn.mock.calls.length).toBe(4); // option 2
+    });
+
+    test('should pow 2 each element', () => {
+        expect(fn.mock.results[0].value).toBe(1); 
+        expect(fn.mock.results[1].value).toBe(4); 
+        expect(fn.mock.results[2].value).toBe(9); 
+        expect(fn.mock.results[3].value).toBe(25); 
     });
 
 })
